1a512e9bf4c21ac12b0d11bd1f282a93bda31cef
Hugo <hugojellerferreira@hotmail.com>
2014-10-03 04:55:45 -0400

    test code - outputing summary and predictions

--- src/polen_classifier/PolenClassifier.java
+++ src/polen_classifier/PolenClassifier.java
@@ -1,24 +1,28 @@
 package polen_classifier;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
 
-import weka.*;
-import surfExtractor.*;
 import surfExtractor.bow_classifier.Bow;
 import surfExtractor.bow_classifier.Histogram;
 import surfExtractor.clustering.Cluster;
 import surfExtractor.clustering.Clustering;
 import surfExtractor.exporter.Exporter;
 import surfExtractor.image_set.ImageSet;
-import surfExtractor.misc.Configuration;
-import surfExtractor.surf_extractor.Main;
 import surfExtractor.surf_extractor.SurfExtractor;
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.SMO;
+import weka.core.FastVector;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+import configuration.Configuration;
 
 public class PolenClassifier {
 	private final static Logger LOGGER = Logger.getLogger(PolenClassifier.class);
@@ -40,17 +44,18 @@ public class PolenClassifier {
 
 	public void run() throws IOException {
 
-		Configuration.addConfiguration("kmeans.kvalue", "768");
-		Configuration.addConfiguration("kmeans.iteration", "5");
-		Configuration.addConfiguration("imageset.path", "C:\\training");
-		Configuration.addConfiguration("arff.path", "c:\\training.arff");
-		Configuration.addConfiguration("arff.relation", "training");
-		Configuration.addConfiguration("random.seed", "1");
-		
-		Configuration.addConfiguration("testimageset.path", "C:\\test");
-		Configuration.addConfiguration("testarff.relation", "testing_arff");
-		Configuration.addConfiguration("testarff.path", "c:\\testing.arff");
-		Configuration.addConfiguration("clusters.path", "c:\\clusters.cluster");
+		Configuration.setConfiguration("kmeans.kvalue", "768");
+		Configuration.setConfiguration("kmeans.iteration", "5");
+		Configuration.setConfiguration("imageset.path", "C:\\training");
+		Configuration.setConfiguration("arff.path", "c:\\training.arff");
+		Configuration.setConfiguration("arff.relation", "training");
+		Configuration.setConfiguration("random.seed", "1");
+
+		Configuration.setConfiguration("testimageset.path", "C:\\test");
+		Configuration.setConfiguration("testarff.relation", "testing_arff");
+		Configuration.setConfiguration("testarff.path", "c:\\testing.arff");
+		Configuration.setConfiguration("clusters.path", "c:\\clusters.cluster");
+		Configuration.debugParameters();
 
 		// Load images from ImageSet
 		ImageSet is = new ImageSet(Configuration.getConfiguration("imageset.path"));
@@ -78,15 +83,18 @@ public class PolenClassifier {
 		 */
 
 		// Cluster all features
-		clustering.cluster();
+		//clustering.cluster();
 
 		// Export clusters
 		LOGGER.info("Saving clusters to file");
-		clustering.saveClustersToFile(new File(Configuration.getConfiguration("clusters.path")));
+		//clustering.saveClustersToFile(new File(Configuration.getConfiguration("clusters.path")));
 
 		// Return final clusters
-		ArrayList<Cluster> featureCluster = clustering.getClusters();
+		//ArrayList<Cluster> featureCluster = clustering.getClusters();
 
+		
+		//Load clusters from poln23e 
+		ArrayList<Cluster> featureCluster = Clustering.loadClustersFromFile(new File("C:\\Users\\Hugo\\Desktop\\clusters.cluster"));
 		// Load Bag Of Words classifier
 		Bow bow = new Bow(is, featureCluster);
 
@@ -105,14 +113,11 @@ public class PolenClassifier {
 		// Write experimental arff
 		Exporter exporter = new Exporter(is, bow);
 		exporter.generateArffFile(Configuration.getConfiguration("arff.path"));
-		
+
 		/*
 		 * 
 		 * TEST ARFF CODE BELOW
-		 * 
 		 */
-		
-		
 
 		// Load images from ImageSet
 		is = new ImageSet(Configuration.getConfiguration("testimageset.path"));
@@ -137,15 +142,14 @@ public class PolenClassifier {
 		LOGGER.info("Saving clusters to file");
 
 		// Load Bag Of Words classifier
-		bow = new Bow(is, new File(Configuration.getConfiguration("clusters.path")));
+		//bow = new Bow(is, new File(Configuration.getConfiguration("clusters.path")));
+		bow = new Bow(is, featureCluster);
 
 		// Compute frequency histograms
 		bow.computeHistograms();
 
 		// Return frequency histograms
 		h = bow.getHistograms();
-
-		// Debug histograms
 		/*
 		 * LOGGER.info("Debugging image histograms"); for (Histogram hh : h) {
 		 * LOGGER.info("Histogram: " + histogramToString(hh)); }
@@ -154,13 +158,44 @@ public class PolenClassifier {
 		// Write experimental arff
 		exporter = new Exporter(is, bow);
 		exporter.generateArffFile(Configuration.getConfiguration("testarff.path"));
-		
+
 		/*
 		 * 
-		 * CLASSIFICATION PART 
-		 * 
+		 * CLASSIFICATION PART
 		 */
-		
+		BufferedReader reader = new BufferedReader(new FileReader(Configuration.getConfiguration("arff.path")));
+		Instances train = new Instances(reader);
+		train.setClassIndex(train.numAttributes() - 1);
+
+		reader = new BufferedReader(new FileReader(Configuration.getConfiguration("testarff.path")));
+		Instances test = new Instances(reader);
+		test.setClassIndex(test.numAttributes() - 1);
+
+		SMO smo = new SMO();
+		try {
+			smo.buildClassifier(train);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Evaluation eval = null;
+		try {
+			eval = new Evaluation(train);
+			eval.evaluateModel(smo, test);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		LOGGER.info(eval.toSummaryString("\nResults\n======\n", false));
+		try {
+			LOGGER.info(eval.toMatrixString());
+			FastVector fv = eval.predictions();
+			for(int i = 0; i < fv.size(); i++) {
+				LOGGER.info(fv.elementAt(i));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 
 	}
